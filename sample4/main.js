768aa2e43dcd4a130d9c519aa69fda83407142bd
otya-ken <82013549+otya-ken@users.noreply.github.com>
2021-11-05 14:32:31 +0900

    Update main.js

--- sample4/main.js
+++ sample4/main.js
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<script>
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioCtx;
 var firstFlag = true;
  
@@ -36,14 +32,13 @@ function onAddFile(event) {
       
       // 曲の秒数
       alert(source.duration + "秒\n"+
-            "サンプリング周波数 : " + source.sampleRate + "\n" +
-            "チャンネル数 : " + source.numberOfChannels);
+            "サンプリング周波数 : " + source.sampleRate);
       
     const sample_frequency = source.sampleRate;
     const num_frames = source.sampleRate * source.duration;
     console.log(sample_frequency);
     console.log(num_frames);
-      console.log(source);      
+    console.log(source);      
       
     }).catch(function(e){
       alert("It is a format not supported.\n"+e);
@@ -55,14 +50,4 @@ function onAddFile(event) {
     document.getElementById("inputfile").value = "";
   }
 }      
-</script>
- 
-</head>
-<body ondrop="onDrop(event);" ondragover="onDragOver(event);">  
-<p></p>
-<table>
-  <tr><th>Audio File</th><td>&nbsp;<input type="file" id="inputfile" onchange="onAddFile(event);"></td></tr>
-</table>    
-<p></p>    
-</body>
-</html>
+
